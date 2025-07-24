ec9cc4dee413a3c37a6cc6bcce6c11ab549be769
DmitryKondarov <162300053+DmitryKondarov@users.noreply.github.com>
2025-07-25 00:10:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int buffer = array[0];
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            array[array.Length-1] = buffer;
+
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
